e91f0ecfb3107c012d0b85ed40c20f03f68e43bf
ErfanDP <delavari.erfan@yahoo.com>
2021-09-13 09:56:02 +0430

    ADD:detect the first time based on Data base and show a dialog FIX:show username error on username already exist error

--- app/src/main/java/me/erfandp/buttondp/view/fragments/LoginFragment.kt
+++ app/src/main/java/me/erfandp/buttondp/view/fragments/LoginFragment.kt
@@ -1,5 +1,6 @@
 package me.erfandp.buttondp.view.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +10,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.erfandp.buttondp.R
+import me.erfandp.buttondp.data.model.User
 import me.erfandp.buttondp.databinding.LoginFragmentBinding
 import me.erfandp.buttondp.utils.extentions.toast
 import me.erfandp.buttondp.viewModels.LoginViewModel
 import me.erfandp.buttondp.viewModels.MainViewModel
+import java.util.*
 
 class LoginFragment: Fragment() {
 	
@@ -34,8 +38,12 @@ class LoginFragment: Fragment() {
 		return binding.root
 	}
 	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		firstTimeActionInit()
+	}
+	
 	private fun initView() {
-		
 		binding.customizableGenericButtonLogin.onClickListener = {
 			onLoginButtonCLicked()
 		}
@@ -43,12 +51,11 @@ class LoginFragment: Fragment() {
 		binding.customizableGenericButtonGotoSignup.onClickListener = {
 			onRegisterButtonClicked()
 		}
-		
 	}
 	
 	
 	private fun onRegisterButtonClicked() {
-		activityViewModel.navigateToSignUp(MainViewModel.NavigationDestinations.Login())
+		navigateToSignup()
 	}
 	
 	
@@ -56,15 +63,12 @@ class LoginFragment: Fragment() {
 		CoroutineScope(Dispatchers.Main).launch {
 			val username = binding.textFieldLoginUsername.editText?.text.toString()
 			val password = binding.textFieldLoginPassword.editText?.text.toString()
-			withContext(Dispatchers.IO){
+			withContext(Dispatchers.IO) {
 				val user = viewModel.checkLoginInfo(username, password)
 				if (user != null) {
 					withContext(Dispatchers.Main) {
 						context?.toast("welcome ${user.fullName}", Toast.LENGTH_SHORT)
-						activityViewModel.navigateToHome(
-							MainViewModel.NavigationDestinations.Login(),
-							user.id
-						)
+						navigateHome(user.id)
 					}
 				} else {
 					withContext(Dispatchers.Main) {
@@ -74,4 +78,42 @@ class LoginFragment: Fragment() {
 			}
 		}
 	}
+	
+	
+	private fun firstTimeActionInit() {
+		CoroutineScope(Dispatchers.IO).launch {
+			if (viewModel.isFirstTime()) {
+				withContext(Dispatchers.Main) {
+					context?.let {
+						MaterialAlertDialogBuilder(it)
+							.setTitle(getString(R.string.login_dialog_welcome))
+							.setMessage(getString(R.string.login_first_time_dialog))
+							.setPositiveButton(getString(R.string.register_now)) { _, _ ->
+								navigateToSignup()
+							}
+							.setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+								dialog.dismiss()
+							}
+							.setCancelable(false)
+							.show()
+					}
+				}
+				
+			}
+		}
+	}
+	
+	private fun navigateHome(userid: UUID) {
+		activityViewModel.navigateToHome(
+			MainViewModel.NavigationDestinations.Login(),
+			userid
+		)
+	}
+	
+	private fun navigateToSignup() {
+		activityViewModel.navigateToSignUp(
+			MainViewModel.NavigationDestinations.Login()
+		)
+	}
+	
 }
